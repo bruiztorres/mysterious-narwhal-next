8e460c81b3e851643426e3bd159c5a8b3e32055c
Borja Ruiz Torres <borja.epsc@gmail.com>
2022-06-17 23:12:42 +0200

    :wrench:

--- src/routes/shop/products/index.ts
+++ src/routes/shop/products/index.ts
@@ -5,6 +5,7 @@ import type { Product } from '$lib/shop/products/types';
 import { dbConnect } from '$lib/core/db';
 import { ProductModel } from '$lib/shop/products/models';
 import { ProductMapper } from '$lib/shop/products/mappers';
+import type { PipelineStage } from 'mongoose';
 
 const projection: PartialRecord<keyof Product, boolean> = {
   name: true,
@@ -16,11 +17,9 @@ const projection: PartialRecord<keyof Product, boolean> = {
 export const get: RequestHandler = async () => {
   await dbConnect();
 
-  const docs = await ProductModel.aggregate([
-    { $project: projection },
-    { $sort: { '_id': -1 } }
-  ]);
-
+  const sort: PipelineStage = { $sort: { _id: -1 } };
+  const project: PipelineStage = { $project: projection };
+  const docs = await ProductModel.aggregate([project, sort]);
   const products = docs.map((product) => ProductMapper.toProductSlim(product));
 
   return { body: { products } };
